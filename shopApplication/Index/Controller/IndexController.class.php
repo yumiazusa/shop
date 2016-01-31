ea0773efdd44016188a206410d2fb9f36a3cb4bc
yumiazusa <yumiazusa@hotmail.com>
2016-01-31 12:24:46 +0800

    second

--- shopApplication/Index/Controller/IndexController.class.php
+++ shopApplication/Index/Controller/IndexController.class.php
@@ -8,8 +8,6 @@ class IndexController extends MyController {
 
 
     public function index(){
-        dump(C('logo'));
-        die;
         //SEO
         $ident ="Indexindex";
         $idents =$this->seo($ident);

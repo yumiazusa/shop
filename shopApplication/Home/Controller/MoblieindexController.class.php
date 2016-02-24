459b5d60160ea2a3541c0ea8b6d40e1f803258fa
yumiazusa <yumiazusa@hotmail.com>
2016-02-24 22:27:29 +0800

    mobile2

--- shopApplication/Home/Controller/MoblieindexController.class.php
+++ shopApplication/Home/Controller/MoblieindexController.class.php
@@ -8,7 +8,24 @@ class MoblieindexController extends MyController {
     }
 	// 首页控制器
     public function Mobileindex(){
-       
+        //SEO
+        $ident ="shopindex";
+        $idents =$this->seo($ident);
+        $this->assign('indexactive','active');
+        $this->assign("title",$idents['title']);
+        $this->assign("keywords",$idents['keywords']);
+        $this->assign("description",$idents['description']);
+
+        // 大图 1366 x 580
+        $modelAdv = M('advs');
+        $m = M();
+        $bannerRes = $modelAdv->field('id, adv_name, thumbnail, product_id,is_connect')
+                              ->where(array('position_id'=>1, 'is_show'=>1))
+                              ->order('id DESC')
+                              ->limit(3)
+                              ->select();
+
+        $this->assign('bannerRes', $bannerRes);
         $this->display();
     }
 
